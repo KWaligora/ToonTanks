4a54f20836f78d3528097f1c78ac5c69bf846d27
Kamil Waligóra <k.waligora37@gmail.com>
2021-08-21 17:37:43 +0200

    Add PawnTank class

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -6,11 +6,32 @@
 #include "PawnBase.h"
 #include "PawnTank.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class TOONTANKS_API APawnTank : public APawnBase
 {
 	GENERATED_BODY()	
+
+private:
+	//Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera;
+
+public:
+	APawnTank();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
